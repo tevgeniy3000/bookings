a9ce8443e2a163aaaedd2a83e888684ae86c5458
tevgeniy3000 <123eugenet@gmail.com>
2022-11-20 11:17:11 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/tevgeniy3000/bookings
+
+go 1.19
+
+require github.com/go-chi/chi v1.5.4
+
+require (
+	github.com/alexedwards/scs/v2 v2.5.0 // indirect
+	github.com/go-chi/chi/v5 v5.0.7 // indirect
+	github.com/justinas/nosurf v1.1.1 // indirect
+)
---
+++ routes.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/tevgeniy3000/bookings/pkg/config"
+	"github.com/tevgeniy3000/bookings/pkg/handlers"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func routes(app *config.AppConfig) http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurve)
+	mux.Use(SessionLoad)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+
+	return mux
+}
