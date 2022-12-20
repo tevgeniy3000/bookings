014db7e224191cf5834cea9e2fde9ac6f0737f25
tevgeniy3000 <123eugenet@gmail.com>
2022-12-20 20:01:00 +0300

    Saving html templates

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -26,7 +26,7 @@ func NewHandlers(r *Repository) {
 
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
 	remoteIP := r.RemoteAddr
-	m.App.Session.Put(r.Context(), "remote)ip", remoteIP)
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
 
 	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
 }
